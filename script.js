5a085f90a29e0f676d83c5a6e942d2a4cfdd02c5
Beata Konya <beatakonya@gmail.com>
2023-12-17 18:35:09 +0000

    added more psuedo coding to the project

--- script.js
+++ script.js
@@ -88,14 +88,29 @@ const upperCasedCharacters = [
   'Z'
 ];
 
-const passwordLength = ;
-const includeLowercase = ;
-const includeUppercase = ;
-const includeSymbols = ;
- 
+const charOptions = []; // it is accessable at more sections 
+const generatedPassword = '';
+// you can store the password as a string and concat each character or
+// as am array and push each character then join once you have enough
+
+
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  //prompt for password length
+  // at least 8 characters no more than 128
+  // conditional to check that the number that was entered is in range
+  // prompts store data as strings so need to parse into a number
+  // if the users input is out of range either return out of the function or call the function again
+
+  // confirm which character sets to use
+  // if the user answers false for all, either return out of the function or call function again
+  // generate a random character for each selected character set
+  // either push selected characters to a mega-array of all selected characters
+  // or you can keep the array seperate   and generate a random number to select the array and another to select the index
+
+
+  // once character sets are sleected move on to generating random characters
 
   let passwordLength =
 
@@ -103,18 +118,31 @@ function getPasswordOptions() {
 
 // Function for getting a random element from an array
 function getRandom(arr) {
- //! Need a variable to hold the password as it's being generated
-
-  //! For loop that loops the number of times that matches the length the user chose
-  //! Generate a random number
-  //! That number is the index for a character in the mega-array
-  //! So then, mega-array[generated-index] is the actual character
-  //! Add that character to the password
+  // need a variable to hold the password as its being generated
+  // need a variable to hold the index that is being generated
   
+  // for loop that loops selectedLenght number of times as many times as the user chose 
+  // generate random number
+  // that number is going to be the index for a character in the mega-array
+  // so then mega-array[generated-index] is the actual character 
+  // add that character to the password 
+
+  //once we finished the for loop return the generated password 
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  // generate a password when the button is clicked
+  //present a series of prompts for password criteria
+  // length of password 
+  // at least 8 characters but no more than 128
+  // character types
+  // lowercase
+  // uppercase
+  // numeric
+  // special characters 
+  // code should validate for each input and at least one character type should be selected
+  // once prompts are answered then the password should be generated and displayed in an alert or written to the page
 
 }
 
